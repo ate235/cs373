8aa8c2024adb5cd15bb9c9b83aabbfec54cccb60
Glenn Downing <downing@cs.utexas.edu>
2013-06-17 12:05:56 -0500

    another commit

---
+++ quizzes/Quiz3.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+"""
+CS373: Quiz #3 (10 pts)
+"""
+
+""" ----------------------------------------------------------------------
+1. In Python, how do you test identity equality vs. content equality?
+   (1 pt)
+
+is vs. ==
+"""
+
+""" ----------------------------------------------------------------------
+2. What is a user story?
+   [XP: Ch. 4, Pg. 24]
+   (2 pts)
+
+a short description of the behavior of the system, from the point of view
+of the user
+"""
+
+""" ----------------------------------------------------------------------
+3. What is a spike solution?
+   [XP: Ch. 6, Pg. 40]
+   (2 pts)
+
+an experiment, where you do enough work to get a good feeling for how to
+implement the story
+"""
+
+""" ----------------------------------------------------------------------
+4. In Java, for the '>>' operator that is defined on 'int', what is
+   the l-value / r-value nature of the return, the left argument, and
+   the right argument?
+   In Python?
+   (2 pts)
+
+r-value, r-value, r-value
+r-value, r-value, r-value
+"""
+
+""" ----------------------------------------------------------------------
+5. In Java, for the '>>=' operator that is defined on 'int', what is
+   the l-value / r-value nature of the return, the left argument, and
+   the right argument?
+   In Python?
+   (2 pts)
+
+r-value, l-value, r-value
+no value, l-value, r-value
+"""
