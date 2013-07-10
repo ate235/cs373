7280a657c1eb13965c7d6d4b5ccb1d9d2b1d2ce6
Glenn Downing <downing@cs.utexas.edu>
2013-07-10 17:08:29 -0500

    another commit

---
+++ quizzes/Quiz11.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+"""
+CS373: Quiz #11 (9 pts)
+"""
+
+""" ----------------------------------------------------------------------
+1. The Self-Adaptive Process
+   The first part of self-adaptivity is regular reviews of the process.
+   Usually you do these with every iteration. At the end of each
+   iteration, have a short meeting and ask yourself the following
+   questions (culled from Norm Kerth)...
+   List any TWO of the FOUR:
+   [The New Methodology]
+   (4 pts)
+
+What did we do well?
+What have we learned?
+What can we do better?
+What puzzles us?
+"""
+
+""" ----------------------------------------------------------------------
+2. List any TWO of the XP design skills in the conclusion.
+   [Is Design Dead?]
+   (5 pts)
+
+A constant desire to keep code as clear and simple as possible.
+
+Refactoring skills so you can confidently make improvements whenever you
+see the need.
+
+A good knowledge of patterns: not just the solutions but also
+appreciating when to use them and how to evolve into them.
+
+Designing with an eye to future changes, knowing that decisions taken
+now will have to be changed in the future.
+
+Knowing how to communicate the design to the people who need to
+understand it, using code, diagrams and above all: conversation.
+"""
