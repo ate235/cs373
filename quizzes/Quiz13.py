53f5d18e5a66db1b807d6b5e5523458dc8c08850
Glenn Downing <downing@cs.utexas.edu>
2013-07-17 17:39:05 -0500

    another commit

---
+++ quizzes/Quiz13.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+"""
+CS373: Quiz #13 (10 pts)
+"""
+
+""" ----------------------------------------------------------------------
+1. What is the origin of the word "scrum"?
+   [SCRUM]
+   (3 pts)
+
+the sport of rugby
+"""
+
+""" ----------------------------------------------------------------------
+2. During a Scrum meeting each team member must answer three questions.
+   Describe any two.
+   [SCRUM]
+   (3 pts)
+
+What have you completed, relative to the backlog, since the last Scrum
+meeting?
+
+What obstacles got in your way of completing this work?
+
+What specific things do you plan to accomplish, relative to the backlog,
+between now and the next Scrum meeting?
+"""
+
+""" ----------------------------------------------------------------------
+3. What is the output of the following function?
+   (3 pts)
+
+{True: 7.8, 'abc': 5.6}
+"""
+
+a = [True, "abc", 1, "def"]
+b = [3.4, 5.6, 7.8]
+print dict(zip(a, b))
